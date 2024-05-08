dbfd450cda2aca016f6abcd692145badfc41e9d6
MariaShulyk <mari.shulyk.05@gmail.com>
2024-05-08 23:24:31 +0200

    1 commit

---
+++ Zad7/Controllers/WarehouseController.cs
@@ -0,0 +1,25 @@
+﻿using Zad7.DTOs;
+using Zad7.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zad7.Controllers;
+
+[ApiController]
+[Route("api/warehouses")]
+public class WarehouseController(IDbService db) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> GetWarehouseId(GetWarehouseDTO warehouseDto)
+    {
+        int id;
+        try
+        {
+            id = await db.GetIdProductWarehouse(warehouseDto);
+        }
+        catch (ArgumentException e)
+        {
+            return NotFound(e.Message);
+        }
+        return Ok(id);
+    }
+}
---
+++ Zad7/DTOs/GetWarehouseDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zad7.DTOs;
+
+public record GetWarehouseDTO(
+    [Required]int IdProduct, 
+    [Required]int IdWarehouse, 
+    [Required]int Amount, 
+    [Required]DateTime CreatedTime
+    );
